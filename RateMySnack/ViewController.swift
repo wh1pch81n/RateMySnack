f8468d2345be169067dc792eec4389aa026777e6
Ho, Derrick <derrick.ho@medtronic.com>
2015-07-10 11:25:09 -0700

    Initial Commit

---
+++ RateMySnack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RateMySnack
+//
+//  Created by Ho, Derrick on 7/10/15.
+//  Copyright (c) 2015 Ho, Derrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
