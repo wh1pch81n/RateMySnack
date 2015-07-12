9c2d51157e5a35b48c9be1f104cbd9e1ff53e04b
Derrick Ho <derrickho328@gmail.com>
2015-07-12 11:36:46 -0700

    new class

---
+++ RateMySnack/Derrick.swift
@@ -0,0 +1,15 @@
+//
+//  Derrick.swift
+//  RateMySnack
+//
+//  Created by Derrick  Ho on 7/12/15.
+//  Copyright (c) 2015 Ho, Derrick. All rights reserved.
+//
+
+import Foundation
+
+class Derrick {
+    func sayHello() {
+        println("Hello")
+    }
+}
