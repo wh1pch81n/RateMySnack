49a74ecdfa64cdbb26d66a7833f72fff8ca8eeec
wh1pch81n <wh1pch81n@gmail.com>
2015-07-18 15:30:37 -0700

    Update BackendDelegate.swift

--- RateMySnack/BackendDelegate.swift
+++ RateMySnack/BackendDelegate.swift
@@ -13,6 +13,7 @@ protocol FormObject {
 }
 
 protocol BackendDelegate {
+    func sharedInstance() -> BackendDelegate;
     func submit(item:FormObject, completionHandler completion:((NSError) -> Void));
     func retrieve(requestCompleted request:((NSError, [FormObject]) -> Void));
 }
