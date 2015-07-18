964465cf645166973e9cf72fe32e3fdae82b66e2
Iceyhan <shantrakan@gmail.com>
2015-07-18 16:23:28 -0700

    backend implemented2

---
+++ RateMySnack/BackEndServer.swift
@@ -0,0 +1,15 @@
+//
+//  BackEndServer.swift
+//  RateMySnack
+//
+//  Created by S. Han on 7/18/15.
+//  Copyright (c) 2015 Ho, Derrick. All rights reserved.
+//
+
+import Foundation
+import Parse
+import Bolts
+
+class BackEndServer: BackendDelegate{
+    
+}
--- RateMySnack/ViewController.swift
+++ RateMySnack/ViewController.swift
@@ -31,13 +31,7 @@ class ViewController: UIViewController {
             println(i)
         }
         
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            println("Object has been saved.")
-        }
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
