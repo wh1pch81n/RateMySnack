94754fe15e5c6e17b283a42004e7198e4415a9c1
Iceyhan <shantrakan@gmail.com>
2015-07-21 17:09:02 -0700

    retieve func added client key

--- RateMySnack/BackEndServer.swift
+++ RateMySnack/BackEndServer.swift
@@ -12,6 +12,8 @@ import Bolts
 
 class BackEndServer: BackendDelegate {
     
+    var objects = [PFObject]()
+    
     static func submit(item: FormObject, completionHandler completion: ((err: NSError?) -> Void)) {
         var snack:PFObject = PFObject(className: "AllSnacks")
         snack["SnackName"] = item.snackName
@@ -29,18 +31,18 @@ class BackEndServer: BackendDelegate {
     static func retrieve(requestCompleted request: ((err: NSError?, objs: [FormObject]) -> Void)) {
         
         var findSnacks:PFQuery = PFQuery(className: "AllSnacks")
+        findSnacks.whereKey("objectId", containsString: "SnackName")
         
         findSnacks.findObjectsInBackgroundWithBlock { (objects: [AnyObject]?, error: NSError?) -> Void in
             if (error == nil){
             
+
                 println("snack info fetched")
                 
-            }else {
-                println(error)
-                println("failed to fetch data")
+            } else {
+                println("failed to fetch")
             }
             
         }
     }
-
 }
