82b745816700aa04238a120bbb5f5cd9bbd5b86f
wh1pch81n <wh1pch81n@gmail.com>
2015-07-28 11:51:32 -0700

    Fixing spacing

--- RateMySnack/ViewController.swift
+++ RateMySnack/ViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class SnackObject : SnackProtocol {
-	var name:String
+    var name:String
     var description:String
     init(name:String, description:String) {
-       self.name = name
+        self.name = name
         self.description = description
     }
 }
