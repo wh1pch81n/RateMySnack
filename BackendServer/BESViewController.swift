0e844b2766584130daba2cefd538186a546d7d07
Iceyhan <shantrakan@gmail.com>
2015-09-29 00:33:04 -0700

    Fully functional mapView but having trouble transferring codes over to the RMSMapViewController

--- BackendServer/BESViewController.swift
+++ BackendServer/BESViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class BESViewController: UIViewController {
+class BESViewController: UIViewController, LocationManagerDelegate, CLLocationManagerDelegate, UITextFieldDelegate{
     
+    @IBOutlet weak var currentLocationTextfield: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    var coreLocationManager = CLLocationManager()
+    var locationManager: LocationManager!
+    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
+        coreLocationManager.delegate = self
+        self.locationManager = LocationManager.sharedInstance
+        self.coreLocationManager.requestAlwaysAuthorization()
+        let authorizationCode = CLLocationManager.authorizationStatus()
+        if authorizationCode == CLAuthorizationStatus.NotDetermined && coreLocationManager.respondsToSelector("requestAlwaysAuthorization") {
+            if NSBundle.mainBundle().objectForInfoDictionaryKey("NSLocationAlwaysUsageDescription") != nil {
+                coreLocationManager.requestAlwaysAuthorization()
+            } else {
+                print("No description provided")
+            }
+            
+        } else {
+         self.getLocation()
+        }
         
         successPopUpOn(self) { (UIAlertAction) -> () in  print("cow a bunga") }
 
@@ -38,6 +59,46 @@ class BESViewController: UIViewController {
             }
         })
     }
+    
+    func displayLocation(location: CLLocation) {
+        mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude), span: MKCoordinateSpanMake(0.05, 0.05)), animated: true)
+        let locationPinCood = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationPinCood
+        
+        mapView.addAnnotation(annotation)
+        mapView.showAnnotations([annotation], animated: true)
+        self.locationManager.reverseGeocodeLocationWithCoordinates(location) { (reverseGecodeInfo, placemark, error) -> Void in
+            let address = reverseGecodeInfo?.objectForKey("formattedAddress") as! String
+            self.currentLocationTextfield.text = address
+        }
+    }
+    
+    func displayEnteredLocation(location: CLLocation) {
+        mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude), span: MKCoordinateSpanMake(0.05, 0.05)), animated: true)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        mapView.showsUserLocation = (status == .AuthorizedAlways)
+        if status != CLAuthorizationStatus.NotDetermined || status != CLAuthorizationStatus.Denied || status != CLAuthorizationStatus.Restricted {
+            getLocation()
+        }
+    }
+    
+    func getLocation() {
+        self.locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) -> () in
+            self.displayLocation(CLLocation(latitude: latitude, longitude: longitude))
+        }
+    }
+    
+    func locationFound(latitude:Double, longitude:Double){
+    }
+    
+    @IBAction func updateCurrentLocationButtonTapped(sender: AnyObject) {
+        getLocation()
+    }
+    
     
 }
 
---
+++ RateMySnack/RMSMapPicker.swift
@@ -0,0 +1,17 @@
+//
+//  RMSMapPicker.swift
+//  RateMySnack
+//
+//  Created by S. Han on 9/27/15.
+//  Copyright © 2015 Ho, Derrick. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+class RMSMapViewController: UIViewController, LocationManagerDelegate{
+    
+    
+    
+}
