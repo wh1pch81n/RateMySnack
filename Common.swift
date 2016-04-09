8af71a500d5403cef2759c65321ddc437957936c
Derrick  Ho <wh1pch81n@gmail.com>
2016-04-09 16:31:30 -0400

    - remove xcconfig constants since they do not work in Xcode 7.3

---
+++ Common.swift
@@ -0,0 +1,20 @@
+//
+//  Common.swift
+//  RateMySnack
+//
+//  Created by Derrick  Ho on 4/9/16.
+//  Copyright © 2016 Ho, Derrick. All rights reserved.
+//
+
+let PARSE_APPLICATION_ID_KEY = "r7EqA8YMRx0ew2OaE2ebKZnErKWdJgmR1hBtQBAQ"
+let PARSE_CLIENT_KEY = "tIWDqmLYrdcquuzznkZS9iJ5CqX8ieOYZhhT4bnz"
+
+let PC_STARRATING = "StarRating"
+let PC_ALLSNACKS = "AllSnacks"
+
+let ALLSNACKS_SNACKNAME = "SnackName"
+let ALLSNACKS_SNACKDESCRIPTION = "SnackDescription"
+
+let STARRATING_ALLSNACKS = "AllSnacks"
+let STARRATING_RATING = "Rating"
+let STARRATING_USER = "User"
--- RateMySnack/RMSStarView.swift
+++ RateMySnack/RMSStarView.swift
@@ -16,32 +16,34 @@ Draws a star on the given context
 :param: fillColor the fill color of the star
 :param: borderColor the border color of the star
 */
-func starDrawing(context: CGContextRef, rect: CGRect)(_ fillColor: UIColor, _ borderColor: UIColor) {
-    // Offsets used to give the star a more star-like look within the containing frame
-    let starArmY = CGRectGetMaxY(rect) * 0.4
-    let starFeetOffset = CGRectGetMaxX(rect) * 0.15
-    let starTopOffset = CGRectGetMaxY(rect) * 0.05
-    let strokeWidth = CGFloat(2.0)
-    
-    // A path for a 5-point star
-    let path: CGMutablePathRef = CGPathCreateMutable()
-    CGPathMoveToPoint(path, nil, CGRectGetMinX(rect) + starFeetOffset, CGRectGetMaxY(rect) - strokeWidth) // BL
-    CGPathAddLineToPoint(path, nil, CGRectGetMidX(rect), CGRectGetMinY(rect) + starTopOffset + strokeWidth) // Top
-    CGPathAddLineToPoint(path, nil, CGRectGetMaxX(rect) - starFeetOffset, CGRectGetMaxY(rect) - strokeWidth) // BR
-    CGPathAddLineToPoint(path, nil, CGRectGetMinX(rect) + strokeWidth, starArmY) // Left
-    CGPathAddLineToPoint(path, nil, CGRectGetMaxX(rect) - strokeWidth, starArmY) // Right
-    CGPathCloseSubpath(path) // connects the last point added with the initial point
-    
-    // Draw the star strokes on to the context
-    CGContextAddPath(context, path)
-    CGContextSetStrokeColorWithColor(context, borderColor.CGColor)
-    CGContextSetLineWidth(context, strokeWidth)
-    CGContextStrokePath(context)
-    
-    // Fill in the star with color on the context
-    CGContextAddPath(context, path)
-    CGContextSetFillColorWithColor(context, fillColor.CGColor)
-    CGContextFillPath(context)
+func starDrawing(context: CGContextRef, rect: CGRect) -> (UIColor, UIColor) -> () {
+	return { (fillColor: UIColor, borderColor: UIColor) -> () in
+		// Offsets used to give the star a more star-like look within the containing frame
+		let starArmY = CGRectGetMaxY(rect) * 0.4
+		let starFeetOffset = CGRectGetMaxX(rect) * 0.15
+		let starTopOffset = CGRectGetMaxY(rect) * 0.05
+		let strokeWidth = CGFloat(2.0)
+		
+		// A path for a 5-point star
+		let path: CGMutablePathRef = CGPathCreateMutable()
+		CGPathMoveToPoint(path, nil, CGRectGetMinX(rect) + starFeetOffset, CGRectGetMaxY(rect) - strokeWidth) // BL
+		CGPathAddLineToPoint(path, nil, CGRectGetMidX(rect), CGRectGetMinY(rect) + starTopOffset + strokeWidth) // Top
+		CGPathAddLineToPoint(path, nil, CGRectGetMaxX(rect) - starFeetOffset, CGRectGetMaxY(rect) - strokeWidth) // BR
+		CGPathAddLineToPoint(path, nil, CGRectGetMinX(rect) + strokeWidth, starArmY) // Left
+		CGPathAddLineToPoint(path, nil, CGRectGetMaxX(rect) - strokeWidth, starArmY) // Right
+		CGPathCloseSubpath(path) // connects the last point added with the initial point
+		
+		// Draw the star strokes on to the context
+		CGContextAddPath(context, path)
+		CGContextSetStrokeColorWithColor(context, borderColor.CGColor)
+		CGContextSetLineWidth(context, strokeWidth)
+		CGContextStrokePath(context)
+		
+		// Fill in the star with color on the context
+		CGContextAddPath(context, path)
+		CGContextSetFillColorWithColor(context, fillColor.CGColor)
+		CGContextFillPath(context)
+	}
 }
 
 @IBDesignable
